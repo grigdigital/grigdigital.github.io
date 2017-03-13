ceeea03d5a97dcd012a5c82aa1ddaba9aec73baa
johndoe@example.com <brunoalv.s@outlook.com>
2017-03-13 03:28:06 -0300

    header, quem somos concluídos. Faltando serviços, clientes, contato, footer e media querie para menu mobile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 'use strict'
 const gulp			= require('gulp');
 const pug			= require('gulp-pug');
-const stylus		= require('gulp-styl');
+const sass			= require('gulp-sass');
 const prefix		= require('gulp-autoprefixer');
 const sourcemaps	= require('gulp-sourcemaps');
-const ghPages		= require('gulp-gh-pages');
+const tinypng		= require('gulp-tinypng');
 const browserSync	= require('browser-sync');
 
 gulp.task('pug', function() {
@@ -15,12 +15,10 @@ gulp.task('pug', function() {
 		.pipe(gulp.dest('./'))
 });
 
-gulp.task('styl', function() {
-	gulp.src('build/assets/stylus/**/*.styl')
+gulp.task('sass', function() {
+	gulp.src('build/assets/sass/**/*.sass')
 		.pipe(sourcemaps.init())
-			.pipe(stylus({
-				compress: true
-			}))
+			.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 			.pipe(prefix({
 				browsers: ['last 2 versions'],
 				cascade: false
@@ -29,23 +27,41 @@ gulp.task('styl', function() {
 		.pipe(gulp.dest('assets/css/'));
 });
 
-gulp.task('deploy', function() {
-	return gulp.src('./**/*')
-		.pipe(ghPages({
-			branch: ['master']
-		}));
-});
-
-gulp.task('browser-sync', ['pug', 'styl'], function() {
+gulp.task('browser-sync', ['pug', 'sass'], function() {
 	browserSync({
 		server: {
 			baseDir: './'
 		}
 	});
 
-	gulp.watch('build/assets/stylus/**/*.styl', ['styl']);
+	gulp.watch('build/assets/sass/**/*.sass', ['sass']);
 	gulp.watch('build/**/*.pug', ['pug']);
-	gulp.watch('./**/*.{html,css,js}').on('change', browserSync.reload);
+	gulp.watch('build/assets/images/**/*.{png,jpg,jpeg}', ['images']);
+	gulp.watch('build/assets/images/**/*.svg', ['svg']);
+	gulp.watch('build/assets/fonts/**/*', ['fonts']);
+	gulp.watch('assets/images/**/*').on('change', browserSync.reload);
+	gulp.watch('assets/**/*.{css,js}').on('change', browserSync.reload);
+	gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('fonts', function() {
+	return gulp.src('build/assets/fonts/**/*')
+		.pipe(gulp.dest('./assets/fonts'))
+});
+
+gulp.task('images', function() {
+	return gulp.src('build/assets/images/**/*.{png,jpg,jpeg}')
+		.pipe(tinypng({
+			key: 'XQVIk6EjMvJX0hY9uhoxgT-DXOxFAtPn',
+			sigFile: 'images/.tinypng-sigs',
+			log: true
+		}))
+		.pipe(gulp.dest('./assets/images/'))
+});
+
+gulp.task('svg', function() {
+	return gulp.src('build/assets/images/**/*.svg')
+		.pipe(gulp.dest('./assets/images/'))
 });
 
 gulp.task('default', ['browser-sync']);
